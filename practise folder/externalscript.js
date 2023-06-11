0646bb18c9c9f950aa29ba283b17ea9a13c70958
anshita-yadav <anshitayadav9614@gmail.com>
2023-06-12 00:29:07 +0530

    javapractise

---
+++ practise folder/externalscript.js
@@ -0,0 +1,35 @@
+//demo on external script
+alert("hello");
+//functio
+function demo() {
+
+    document.write("hello javascript <br/>");
+}
+
+function demoA() {
+
+    document.write("This is External script <br/>");
+}
+
+function fruit() {
+
+    document.write("apple,banana,grapes <br/>");
+}
+
+function num() {
+
+    document.write(1 + 20, 50 - 6, 55 * 2);
+}
+
+function fun1() {
+
+    document.write("anshitayadav");
+}
+
+function sum() {
+    document.write(50 + 100000);
+}
+
+
+
+
