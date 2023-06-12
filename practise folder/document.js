1ea1a13da8fc6c81a8e25959821fde12c2686d5d
anshita-yadav <anshitayadav9614@gmail.com>
2023-06-13 02:52:27 +0530

    doucment

---
+++ practise folder/document.js
@@ -0,0 +1,55 @@
+function demo() {
+    document.write("anshita <br/>");
+}
+
+function demoone() {
+    document.write("ayu" + "yadav <br/>")
+}
+
+function demotwo() {
+
+    document.write("Hello World This is doucment <br/>");
+}
+
+function demothree() {
+
+    document.write("sum ")
+    document.write(50 + 60);
+}
+
+function demofour() {
+
+    document.write(500 * 55);
+}
+
+//console
+
+function demofive() {
+
+    console.log("hello" + "anshita");
+
+}
+
+function demosix() {
+    console.log("This is console log");
+}
+
+function demoseven() {
+
+    console.log(50 + 6000);
+}
+
+function demoeight() {
+
+    console.log(5000 * 85);
+}
+
+function demonine() {
+
+    console.log(5000 / 6);
+}
+
+function demoten() {
+
+    console.log(6245 - 5982);
+}
