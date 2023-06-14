9fa30024df94e00818513c30e4b454ee10b5a824
anshita-yadav <anshitayadav9614@gmail.com>
2023-06-15 00:38:25 +0530

    simpleloginpage

---
+++ practise folder/javascript/simpleloginpage.js
@@ -0,0 +1,23 @@
+//demo on html tag 
+document.write("<h1>Login page</h1>");
+
+document.write('<form action="https://www.google.com/">');
+
+document.write("<label>Username</label>");
+
+document.write('<input type="text" id="usn" name="usn"/>');//" " " "error
+document.write("<br/>");
+
+document.write("<label>Password</label>");
+
+document.write('<input type="password" id="pass" name="pass"/>');
+document.write("<br/>");
+
+document.write('<label>Email</label>');
+
+document.write('<input type="email" id="mail" name="mail"/>');
+document.write("<br/>");
+
+document.write("<button>Login</button>")
+
+document.write("</form>")
---
+++ practise folder/javascript/variable.js
@@ -0,0 +1,112 @@
+//demo on variable
+
+//three tyoes of variable var let const
+
+//var
+
+var person = "riya";//string
+age = 25;//number
+gender = "female";//string
+isIndian = true;//boolean
+
+document.write(person, "<br>");
+document.write(age + " " + gender, "<br/>");
+
+document.write(isIndian, "<br>");
+
+var person = "ankita";
+
+age = 26;
+gender = "female";
+
+document.write(person + age + gender, "<br>");
+
+var person = "uday";
+age = 30;
+gender = "male";
+
+
+document.write(person + gender, "<br>");
+
+document.write(age, "<br/>");
+
+//let
+
+let user = 'anshita';
+age = 20;
+subject = "HTML";
+
+console.log(user + age);
+console.log(user);
+
+let username = 'ayushi';
+age = 26;
+subject = "CSS";
+salary = 10000;
+document.write(username, "<br/>");
+
+let userinfo = "ram"
+salary = 2000;
+subject = "JAVASCRIPT";
+country = "india";
+
+document.write(userinfo + " " + salary, "<br/>");
+
+document.write(subject + " " + country, "<br/>");
+
+let userdeatils = "priya";
+salary = 4000;
+subject = "REACT";
+country = "malasiya";
+
+document.write(userdeatils, "<br/>");
+
+document.write(salary + " " + subject + " " + country, "<br/>");
+
+//const (upper case /capital letter/no delcartion)
+//const value not changed
+
+const a = 500;//initilization
+b = 589;
+c = a + b;
+
+document.write(c, "<br/>");
+
+const x = 9643;
+y = 569;
+mult = x + y;
+
+document.write(mult, "<br/>");
+
+const sub = 560 * 5;
+
+document.write(sub, "<br/>");
+
+const div = 635248 / 55;
+
+document.writeln(div);
+
+const persondetail = "priya";
+subject = "PAYTHON";
+
+country = "US";
+salary = 5630;
+
+console.log(persondetail);
+
+console.log(salary + " " + country);
+
+console.log(subject);
+
+
+const personinfo = "tilak";
+
+age = 28;
+salary = 6666;
+
+city = "hyderabad";
+
+console.log(personinfo);
+
+console.log(salary + "  " + city);
+console.log(age);
