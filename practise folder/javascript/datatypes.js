52f207ee7d3cd4814e517899e1dedb753ccc3a27
anshita-yadav <anshitayadav9614@gmail.com>
2023-06-16 01:55:08 +0530

    inputtype

---
+++ practise folder/javascript/datatypes.js
@@ -0,0 +1,150 @@
+//demo on datatypes(js did not provide any data type)
+
+//primitive datatypes
+
+//string
+
+let pername = "anshita 20 female <br/>";
+document.write(pername, "<br/>");
+
+
+let user = "ankita 28 female <br/>";
+
+document.write(user);
+
+let userName = "uday 30 female <br/>";
+
+document.write(userName);
+
+let userInfo = "ayu 26 indian <br/>";
+
+document.write(userInfo);
+
+let userdeatils = "tilak 29 US <br/>";
+
+document.write(userdeatils);
+
+let userId = "aathrav 3 male <br/>";
+
+document.write(userId);
+
+
+const A = "anshita yadav <br/>";
+
+document.write(A);
+
+const X = "aarush yadav <br/>";
+
+document.write(X);
+
+const Text = '"This is const variable "<br/>';
+
+document.write(Text);
+
+const Backtick = `riya
+UK`;
+
+document.write(Backtick);
+
+//number
+
+let a = 50 + 649;
+
+console.log(a);
+
+let sum = 562 + 98634;
+
+console.log(sum);
+
+let mult = 55 * 92;
+
+console.log(mult);
+
+let sub = 569 - 53;
+
+console.log(sub);
+
+let div = 5698 / 64;
+
+console.log(div);
+
+//undefine
+
+let x;
+
+console.log(x);
+
+let y;
+console.log(y);
+
+y = 20;
+console.log(y);
+
+//null
+
+let num = null;
+
+console.log(num);
+
+let age = null;
+
+console.log(age);
+
+//typeof
+
+let one = "siva";
+console.log(one);
+console.log(typeof one);
+
+let two = 50;
+
+console.log(two, "=>", typeof two);
+
+let three = true;
+
+console.log(three, "=>", typeof three);
+
+let four = "priya 20 true";
+
+console.log(four, "=>", typeof four);
+
+
+let five;
+
+console.log(five, "=>", typeof five);
+
+//non primitive data types
+
+//Array
+
+const Array = ["vaishu", "diksha"];
+
+console.log([Array]);
+
+console.log([Array.length]);
+
+console.log(Array, "=>", typeof Array);
+
+const Arraysub = ["HTML", "CSS", "JAVASCRIPT", "REACT", "PYTHON", "ANGULAR REACT"];
+
+console.log(Arraysub.length);
+
+console.log(Arraysub, "=>", typeof Arraysub);
+
+//class(in class const write in small letter)
+
+const n = new Number();
+
+console.log(n);
+
+const b = new Boolean();
+
+console.log(b);
+
+const st = new String("javascript");
+
+console.log(st);
+
+const text = new String(`This is a class non primitive data type`);
+
+console.log(text);
