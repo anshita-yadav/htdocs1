93d1ae8a59d05ad474416bf9afbd8a65ef135cca
anshita-yadav <anshitayadav9614@gmail.com>
2023-06-23 01:49:43 +0530

    loops

---
+++ practise folder/javascript/forloop.js
@@ -0,0 +1,122 @@
+for (i = 1; i <= 5; i++) {
+    document.write("This is" + i + "<br/>")
+}
+
+
+for (i = 1; i <= 20; i++) {
+    document.write(i + "<br/>");
+}
+
+document.write("<br/>");
+
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
+
+for (i = 1; i <= 10; i++) {
+    document.write(i + "<br/>");
+}
+
+for (i = 1; i <= 20; i++) {
+    document.write(i + "<br/> ");
+}
+
+for (i = 1; i <= 50; i++) {
+    document.write(i + " <br/>");
+}
+
+for (i = 2; i <= 30; i++) {
+    document.write(i + " <br/>");
+}
+
+for (i = 3; i <= 60; i++) {
+    document.write(i + " <br/>");
+}
+
+for (i = 20; i <= 30; i++) {
+    document.write(i + "<br/> ");
+}
+
+for (i = 0; i <= 9; i++) {
+    document.write(i + "<br/> ");
+}
+for (i = 30; i <= 40; i++) {
+    console.log(i + " ");
+}
+
+for (i = 0; i <= 15; i++) {
+    console.log("NUM" + i + " ");
+}
+
+for (i = 20; i >= 1; i--) {
+    console.log(i + " ");
+}
+
+for (i = 100; i >= 90; i--) {
+    console.log(i + " ");
+}
+
+for (i = 80; i >= 70; i--) {
+    console.log(i + " ");
+}
+
+for (i = 15; i >= 1; i--) {
+    console.log(i + " ");
+}
+
+
+for (i = 60; i >= 55; i--) {
+    console.log(i + " ");
+}
+
+for (i = 30; i >= 11; i--) {
+    console.log(i + " ");
+}
+
+for (i = 5; i >= 0; i--) {
+    console.log(i + " ");
+}
+
+for (i = 40; i >= 35; i--) {
+    console.log(i + " ");
+}
+
+
+for (i = 20; i >= 1; i--) {
+    console.log("Hello" + i + " ");
+}
+
+for (i = 50; i <= 48; i--) {
+    console.log(i + " ");
+}
+
+for (i = 20; i >= 11; i--) {
+    console.log(i + " ");
+}
+
+for (i = 55; i >= 40; i--) {
+    console.log(i + " ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
