890d90eaf6db6f4067c977147584d65b4d4cfaab
anshita-yadav <anshitayadav9614@gmail.com>
2023-08-15 00:20:22 +0530

    Login

---
+++ reactapp/src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <>
+      <form action="#">
+        <div className="main-content">
+          <h1 className="head-content">Login Page</h1>
+
+          <div className="box-content">
+            <div className="margin-content">
+              <label>Name</label>
+              <input type="text" className="flex-content" />
+            </div>
+            <div className="margin-content">
+              <label>Surname</label>
+              <input
+                type="text"
+                id="sur"
+                name="sunm"
+                className="flex-content"
+              />
+            </div>
+
+            <div className="margin-content">
+              <label>Password</label>
+              <input
+                type="password"
+                id="pass"
+                name="pass"
+                className="flex-content"
+              />
+            </div>
+
+            <div className="margin-content">
+              <label>Email</label>
+              <input
+                type="email"
+                id="mail"
+                name="mail"
+                className="flex-content"
+              />
+            </div>
+
+            <div className="margin-content">
+              <input
+                type="submit"
+                value="Login"
+                className="but-content flex-content"
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export { Login };
